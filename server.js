cb25d1745f473c3fa775299d95f2404f6ac15e94
202296 <mouhamedkoko996@gmail.com>
2023-09-16 22:48:50 +0000

    remove

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-// const dbConnect = require('./src/configs/connectDB');
 const App = express();
-const dotenv = require('dotenv').config();
-// dbConnect()
 
-App.use('/', require('./src/routes/path'));
+App.use('/', require('./routes/path'));
 
-const port = 8080;
+const port = 3000;
 
 App.listen(process.env.PORT || port);
 console.log(`Web Server is listening at: ${process.env.PORT || port}`);
